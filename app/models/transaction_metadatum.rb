b498d899db96a1be775c0a6c64345cfb1c1d1e7a
Cameron Price <cameron.prc@gmail.com>
2019-02-18 20:36:43 +1300

    Updated transaction meta_datam to pass tests

--- app/models/transaction_metadatum.rb
+++ app/models/transaction_metadatum.rb
@@ -1,3 +1,12 @@
 class TransactionMetadatum < ApplicationRecord
-  belongs_to :transaction_object, foreign_key: 'transaction_id', class_name: 'Transaction', :inverse_of => :transaction_metadata, dependent: :destroy
+
+  belongs_to :transaction_object,
+             foreign_key: 'transaction_id',
+             class_name: 'Transaction',
+             :inverse_of => :transaction_metadata,
+             dependent: :destroy
+
+  validates :field, presence: true
+  validates :value, presence: true
+
 end
