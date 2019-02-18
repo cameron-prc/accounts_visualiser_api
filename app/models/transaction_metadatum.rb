e2c5db45bc147296a1e6f3ef574fa6d7c08d9bc7
Cameron Price <cameron.prc@gmail.com>
2019-02-18 19:59:12 +1300

    Added destroy dependancy

--- app/models/transaction_metadatum.rb
+++ app/models/transaction_metadatum.rb
@@ -1,3 +1,3 @@
 class TransactionMetadatum < ApplicationRecord
-  belongs_to :transaction_object, foreign_key: 'transaction_id', class_name: 'Transaction', :inverse_of => :transaction_metadata
+  belongs_to :transaction_object, foreign_key: 'transaction_id', class_name: 'Transaction', :inverse_of => :transaction_metadata, dependent: :destroy
 end
