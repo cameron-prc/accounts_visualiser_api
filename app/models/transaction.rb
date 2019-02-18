6b94269a6ff7962d714a4ed656d226767915344a
Cameron Price <cameron.prc@gmail.com>
2019-02-18 19:07:15 +1300

    Added model validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,8 @@
 class Transaction < ApplicationRecord
+
+  validates :amount, presence: true
+  validates :details, presence: true
+  validates :date, presence: true
+  validates :transaction_type, presence: true
+
 end
