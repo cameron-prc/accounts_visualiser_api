567639c4add16cf2860b3a40c2112b743e61d28e
Cameron Price <cameron.prc@gmail.com>
2019-02-26 20:32:02 +1300

    Added basic test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+start_date = Date.today - 1.month
+end_date = Date.today
+count = 0
+
+(start_date..end_date).each do |date|
+
+  (0..4).each do
+
+    amount = rand(10000) * rand
+
+    Transaction.new({ amount: amount.round(2), details: "Details_#{count}", date: date, transaction_type: "Transaction type" }).save!
+
+    count += 1
+
+  end
+
+end
