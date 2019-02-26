55035a20c7bc3d351e7fe7b28e18a0d74a64d810
Cameron Price <cameron.prc@gmail.com>
2019-02-26 20:52:52 +1300

    Added a basic search functionality to transactions with start data and end date as paramaters

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,4 +7,14 @@ class Transaction < ApplicationRecord
   validates :date, presence: true
   validates :transaction_type, presence: true
 
+
+  def self.search(args)
+
+    start_date = args[:start_date] || (Date.today - 3.months).strftime("%Y-%m-%d")
+    end_date = args[:end_date] || Date.today.strftime("%Y-%m-%d")
+
+    Transaction.where(date: start_date..end_date)
+
+  end
+
 end
