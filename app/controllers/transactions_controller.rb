55012c65b55bf675e1b2fe2351c0fdc10f1dc103
Cameron Price <cameron.prc@gmail.com>
2019-02-26 20:53:46 +1300

    Added transaction controller

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,50 @@
+class TransactionsController < ApplicationController
+  before_action :set_transaction, only: [:show, :update, :destroy]
+
+  def index
+
+    @transactions = Transaction.search(transaction_search_params)
+
+    render json: @transactions
+  end
+
+  def show
+    render json: @transaction
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+
+    if @transaction.save
+      render json: @transaction, status: :created, location: @transaction
+    else
+      render json: @transaction.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @transaction.update(transaction_params)
+      render json: @transaction
+    else
+      render json: @transaction.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @transaction.destroy
+  end
+
+  private
+
+    def set_transaction
+      @transaction = Transaction.find(params[:id])
+    end
+
+    def transaction_params
+      params.require(:transaction).permit( :date, :amount, :details )
+    end
+
+    def transaction_search_params
+      params.permit( :start_date, :end_date )
+    end
+end
